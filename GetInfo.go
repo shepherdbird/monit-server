6a7c60471055fd8b4712d2e3371ce5e7129c6138
dawei <dawei@dawei-OptiPlex-3020.(none)>
2015-07-08 16:43:15 +0800

    complete

--- GetInfo.go
+++ GetInfo.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	//"time"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Net struct {
+	Name string
+	Rx   float64
+	Tx   float64
+}
+type Config struct {
+	Timestamp      int64
+	Cpuusage       uint64
+	Cpufrequency   uint64
+	Cpucores       int
+	Memmoryusage   uint64
+	Memorycapacity int64
+	Diskusage      uint64
+	Diskcapacity   uint64
+	NetworkInfo    []*Net
+}
+type Nodestatus struct {
+	Ip        string
+	CpuMax    uint64
+	MemoryMax uint64
+	DiskMax   uint64
+	RxMax     float64
+	TxMax     float64
+	Status    []*Config
+	Point     int
+}
+
+func NewNodestatus(ip string) *Nodestatus {
+	conf := []*Config{}
+	return &Nodestatus{
+		Ip:        ip,
+		CpuMax:    0,
+		MemoryMax: 0,
+		DiskMax:   0,
+		RxMax:     0,
+		TxMax:     0,
+		Status:    conf,
+		Point:     0,
+	}
+}
 func (c *Config) GetMachineInfo(ip string) {
 	machineinfo := MachineInfo{}
 	client := &http.Client{}
@@ -22,19 +70,28 @@ func (c *Config) GetMachineInfo(ip string) {
 }
 func (c *Config) GetContainerInfo(ip string) {
 	containerinfo := ContainerInfo{}
-	resp, err = client.Get("http://" + ip + ":4194/api/v1.3/containers/")
+	client := &http.Client{}
+	resp, err := client.Get("http://" + ip + ":4194/api/v1.3/containers/")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	defer resp.Body.Close()
+	buff := new(bytes.Buffer)
 	buff.ReadFrom(resp.Body)
 	_ = json.Unmarshal(buff.Bytes(), &containerinfo)
-	c.Timestamp = containerinfo.Spec.CreationTime
-	c.Cpuusage = (containerinfo.Stats[len(containerinfo.Stats)-1].Cpu.Usage.Total - containerinfo.Stats[0].Cpu.Usage.Total) / (len(containerinfo.Stats) - 1)
+	//c.Timestamp = containerinfo.Spec.CreationTime.Unix()
+	c.Timestamp = containerinfo.Stats[len(containerinfo.Stats)-1].Timestamp.Unix()
+	interval := containerinfo.Stats[len(containerinfo.Stats)-1].Timestamp.Sub(containerinfo.Stats[0].Timestamp)
+	c.Cpuusage = uint64(float64(containerinfo.Stats[len(containerinfo.Stats)-1].Cpu.Usage.Total-containerinfo.Stats[0].Cpu.Usage.Total) / float64(interval) * 1000000000)
 	var Filesystem uint64 = 0
 	for _, fs := range containerinfo.Stats[0].Filesystem {
 		Filesystem += fs.Usage
 	}
 	c.Diskusage = Filesystem
 	c.Memmoryusage = containerinfo.Stats[0].Memory.Usage
+	net := &Net{}
+	net.Name = containerinfo.Stats[0].Network.Name
+	net.Rx = float64(containerinfo.Stats[len(containerinfo.Stats)-1].Network.RxBytes-containerinfo.Stats[0].Network.RxBytes) * 1000 / float64(interval)
+	net.Tx = float64(containerinfo.Stats[len(containerinfo.Stats)-1].Network.TxBytes-containerinfo.Stats[0].Network.TxBytes) * 1000 / float64(interval)
+	c.NetworkInfo = append(c.NetworkInfo, net)
 }
--- monitServer.go
+++ monitServer.go
@@ -9,53 +9,61 @@ import (
 	"time"
 )
 
-type Net struct {
-	Name string
-	Rx   float64
-	Tx   float64
-}
-type Config struct {
-	Timestamp      time.Time
-	Cpuusage       uint64
-	Cpufrequency   uint64
-	Cpucores       int
-	Memmoryusage   uint64
-	Memorycapacity uint64
-	Diskusage      uint64
-	Diskcapacity   uint64
-	NetworkInfo    []Net
-}
-type NodeStatus struct {
-	Ip     string
-	Status []Config
-}
-
 var (
 	Ips     []string
-	Cluster []NodeStatus
+	Cluster map[string]*Nodestatus
 )
 
 func status(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("%v", Ips)
-	Jdata, _ := json.Marshal(Ips)
+	Jdata, _ := json.Marshal(Cluster)
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(Jdata))
+
 }
 func getStatus() {
-
-	time.Sleep(time.Second * 30)
+	for {
+		for _, ip := range Ips {
+			conf := &Config{}
+			conf.GetMachineInfo(ip)
+			conf.GetContainerInfo(ip)
+			if Cluster[ip].CpuMax < conf.Cpuusage {
+				Cluster[ip].CpuMax = conf.Cpuusage
+			}
+			if Cluster[ip].DiskMax < conf.Diskusage {
+				Cluster[ip].DiskMax = conf.Diskusage
+			}
+			if Cluster[ip].MemoryMax < conf.Memmoryusage {
+				Cluster[ip].MemoryMax = conf.Memmoryusage
+			}
+			if Cluster[ip].RxMax < conf.NetworkInfo[0].Rx {
+				Cluster[ip].RxMax = conf.NetworkInfo[0].Rx
+			}
+			if Cluster[ip].TxMax < conf.NetworkInfo[0].Tx {
+				Cluster[ip].TxMax = conf.NetworkInfo[0].Tx
+			}
+			if len(Cluster[ip].Status) < 240 {
+				Cluster[ip].Status = append(Cluster[ip].Status, conf)
+			} else {
+				Cluster[ip].Status[Cluster[ip].Point] = conf
+			}
+			Cluster[ip].Point = (Cluster[ip].Point + 1) % 240
+		}
+		time.Sleep(time.Second * 30)
+	}
 
 }
 func main() {
-	getStatus()
+	go getStatus()
 	http.HandleFunc("/api/status", status)
-	err := http.ListenAndServe("127.0.0.1:50000", nil)
+	err := http.ListenAndServe("0.0.0.0:50000", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err.Error())
 	}
 }
 func init() {
 	Ips = []string{}
-	Cluster = []NodeStatus{}
+	Cluster = map[string]*Nodestatus{}
 	data := NodeList{}
 	client := &http.Client{}
 	resp, err := client.Get("http://10.10.103.250:8080/api/v1beta3/nodes/")
@@ -68,6 +76,6 @@ func init() {
 	_ = json.Unmarshal(buff.Bytes(), &data)
 	for _, item := range data.Items {
 		Ips = append(Ips, item.Name)
-		fmt.Println(item.Name)
+		Cluster[item.Name] = NewNodestatus(item.Name)
 	}
 }
